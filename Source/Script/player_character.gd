1e8a3227f57413e8c0c0150bafd6aa26910c9c5d
Crekpozer <ereck@live.com>
2025-05-15 17:56:33 -0300

    Adição do pulo duplo

--- Source/Script/player_character.gd
+++ Source/Script/player_character.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 # Velocidade do movimento no ar
 @export var airMoveSpeed : float = 20
 # Angulo de inclinação
-@export var tiltAmount : float = 0.4
+@export var tiltAmount : float = 0.2
 # Gravidade padrão
 @export var gravity : float = 9.8
 # Força do pulo
@@ -15,16 +15,20 @@ extends CharacterBody3D
 var jumpDirection : Vector3 = Vector3.ZERO
 # Indica se o jogador pulou quanto estava andando
 var jumpFromMoving : bool = false
+# Indica se o personagem pode dar o pulo duplo ou não
+var canDoubleJump : bool = true
 
-# Referencia a camera
+# Referencia ao nodo da camera
 var cameraFollow : Node3D
 
+# Função que dispara quando o personagem é carregado na cena
 func _ready() -> void:
+	# Preenche a varivel com a referencia do nodo da camera
 	cameraFollow = get_parent().get_node("CameraFollow")
 
 # Função que lida com a fisica do personagem
 func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO # Direção 
+	var direction = Vector3.ZERO # Direção zerada
 	var tilt = 0.0 # Inclinação inicial
 	
 	# Aplica a gravidade
@@ -32,12 +36,13 @@ func _physics_process(delta: float) -> void:
 	
 	# Se o personagem estiver no chão
 	if is_on_floor():
+		canDoubleJump = true
 		# Detecta o input do jogador e adiciona o movimento
 		if Input.is_action_pressed("left"):
 			direction.x -= 1 # faz com que a direção do personagem seja -1 no eixo x
-			tilt = tiltAmount # Inclina o personagem na direção que o jogador está andando
-			transform.basis = Basis(Vector3(0, 1, 0), deg_to_rad(180))
-			cameraFollow.global_position = global_position
+			tilt = -tiltAmount # Inclina o personagem na direção que o jogador está andando
+			transform.basis = Basis(Vector3(0, 1, 0), deg_to_rad(180)) # Gira o personagem quando indo pra esquerda
+			cameraFollow.global_position = global_position # Ajusta a camera em relação a rotação do personagem
 		if Input.is_action_pressed("right"):
 			direction.x += 1 # Faz com que a direção do personagem seja 1 no eixo x
 			tilt = -tiltAmount # Inclina o personagem na direção que o jogador está se movimentando
@@ -45,7 +50,7 @@ func _physics_process(delta: float) -> void:
 		
 		# Atualizar direção do pulo SOMENTE quando estiver no chão
 		if direction.x != 0:
-			jumpDirection.x = direction.x * 0.1	
+			jumpDirection.x = direction.x * 0.1 # 
 			jumpFromMoving = true
 		else:
 			jumpFromMoving = false
@@ -54,9 +59,14 @@ func _physics_process(delta: float) -> void:
 		jumpFromMoving = direction.x != 0
 	
 	# Faz o personagem pular
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = jumpForce
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpForce # Joga o personagem para cima
+		# Verifica se o personagem está em movimento ou não e aplica a velocidade no eixo x adequada para a situação
 		velocity.x = jumpDirection.x * moveSpeed if jumpFromMoving else jumpDirection.x * airMoveSpeed
+	
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and canDoubleJump:
+		velocity.y = jumpForce
+		canDoubleJump = false
 	
 	# Permite movimentação reduzida no ar
 	if not is_on_floor() and not jumpFromMoving:
@@ -71,4 +81,5 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide() # Aplicao movimento no personagem
 	
-	rotation = rotation.lerp(Vector3(rotation.x, rotation.y, tilt), 0.1) # inclina levemente o personagem
+	# inclina levemente o personagem para dar a impressão de movimento
+	rotation = rotation.lerp(Vector3(rotation.x, rotation.y, tilt), 1.0)
